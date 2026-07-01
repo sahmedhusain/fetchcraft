186c4542a4fb7747021643669f659cf25a669acb
Sayed Ahmed Husain <160347327+sahmedhusain@users.noreply.github.com>
2026-07-01 04:49:28 +0300

    feat: implement background download flag -B

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 
 	"wget/src/config"
 	"wget/src/download"
@@ -19,6 +20,29 @@ func main() {
 		fmt.Println("wget: missing URL")
 		fmt.Println("Usage: go run . [options] <URL>")
 		os.Exit(1)
+	}
+
+	if cfg.Background && os.Getenv("WGET_BACKGROUND_CHILD") != "1" {
+		logFile, err := os.OpenFile("wget-log", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating log file: %v\n", err)
+			os.Exit(1)
+		}
+		defer logFile.Close()
+
+		cmd := exec.Command(os.Args[0], os.Args[1:]...)
+		cmd.Env = append(os.Environ(), "WGET_BACKGROUND_CHILD=1")
+		cmd.Stdout = logFile
+		cmd.Stderr = logFile
+
+		err = cmd.Start()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error starting background process: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println("Output will be written to ‘wget-log’.")
+		os.Exit(0)
 	}
 
 	if len(cfg.URLs) > 0 {
